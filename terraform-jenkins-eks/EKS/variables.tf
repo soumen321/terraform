71a8068995a59849e7666cb60437a9139166df14
soumen321 <bhattacharjee.soumen@gmail.com>
2024-05-25 22:03:47 +0530

    Create EKS using Terraform with Jenkins

---
+++ terraform-jenkins-eks/EKS/variables.tf
@@ -0,0 +1,12 @@
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+}
+variable "private_subnets" {
+  description = "Subnet CIDR"
+  type        = list(string)
+}
+variable "public_subnets" {
+  description = "Subnet CIDR"
+  type        = list(string)
+}
---
+++ terraform-jenkins-eks/jenkins-server/data.tf
@@ -0,0 +1,16 @@
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-*-hvm-*-x86_64-gp2"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_availability_zones" "azs" {
+  state = "available"
+}
