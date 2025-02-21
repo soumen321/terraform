0ede084a2d246ce8f4c33893e4f7f010a6d5d98c
Soumen Bhattacharjee <bhattacharjee.soumen@gmail.com>
2025-02-21 09:14:42 +0530

    Update outputs.tf

--- impl_dynamo_3/outputs.tf
+++ impl_dynamo_3/outputs.tf
@@ -1,7 +1,7 @@
-# output "dynamodb_table_names" {
-#   value = { for k, v in module.dynamodb : k => v.dynamodb_table_name }
-# }
+ output "dynamodb_table_names" {
+   value = { for k, v in module.dynamodb : k => v.dynamodb_table_name }
+ }
 
-# output "dynamodb_table_arns" {
-#   value = { for k, v in module.dynamodb : k => v.dynamodb_table_arn }
-# }
+ output "dynamodb_table_arns" {
+   value = { for k, v in module.dynamodb : k => v.dynamodb_table_arn }
+ }
